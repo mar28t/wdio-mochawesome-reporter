eea37c79d2cca190c90a39ef3f011a79dcbc3017
mar28t <malvina.khv@gmail.com>
2023-10-23 19:20:47 +0300

    type module

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,114 +1,146 @@
-import WdioMochawesomeReporter from '../src/index'
+import WdioMochawesomeReporter from "../src/index.js";
 
-describe('Reporter Tests',()=>{
+describe("Reporter Tests", () => {
     const runner = {
-        sanitizedCapabilities: 'chrome',
-        sessionId: '123456'
-    }
-    let reporter
+        sanitizedCapabilities: "chrome",
+        sessionId: "123456",
+    };
+    let reporter;
 
-    beforeEach(()=>{
-        reporter = new WdioMochawesomeReporter({})
-        reporter.onRunnerStart(runner)
-    })
+    beforeEach(() => {
+        reporter = new WdioMochawesomeReporter({});
+        reporter.onRunnerStart(runner);
+    });
 
-    it('onRunnerStart',()=>{
+    it("onRunnerStart", () => {
         expect(reporter.results).toMatchObject({
             copyrightYear: expect.anything(),
             stats: expect.anything(),
-            suites: expect.anything()
-        })
-        expect(reporter.sanitizedCaps).toBe(runner.sanitizedCapabilities)
-        expect(reporter.sessionId).toBe(runner.sessionId)
-    })
+            suites: expect.anything(),
+        });
+        expect(reporter.sanitizedCaps).toBe(runner.sanitizedCapabilities);
+        expect(reporter.sessionId).toBe(runner.sessionId);
+    });
 
-    it('onSuiteStart',()=>{
-        const suite = {title: 'sample suite'}
+    it("onSuiteStart", () => {
+        const suite = { title: "sample suite" };
 
-        reporter.onSuiteStart(suite)
-        expect(reporter.results.stats.suites).toBe(1)
-        expect(reporter.currSuite.title).toBe(`${suite.title} (${runner.sanitizedCapabilities})`)
-    })
+        reporter.onSuiteStart(suite);
+        expect(reporter.results.stats.suites).toBe(1);
+        expect(reporter.currSuite.title).toBe(
+            `${suite.title} (${runner.sanitizedCapabilities})`
+        );
+    });
 
-    it('onTestStart',()=>{
-        const suite = {title: 'sample suite',uuid: '1234'}
-        const test = {title: 'this is a test',uuid: '9876'}
+    it("onTestStart", () => {
+        const suite = { title: "sample suite", uuid: "1234" };
+        const test = { title: "this is a test", uuid: "9876" };
 
-        reporter.onSuiteStart(suite)
-        reporter.onTestStart(test)
-        expect(reporter.currTest.title).toBe(test.title)
-        expect(reporter.currTest.context[0]).toMatchObject({ title: 'Session Id', value: runner.sessionId })
-    })
+        reporter.onSuiteStart(suite);
+        reporter.onTestStart(test);
+        expect(reporter.currTest.title).toBe(test.title);
+        expect(reporter.currTest.context[0]).toMatchObject({
+            title: "Session Id",
+            value: runner.sessionId,
+        });
+    });
 
-    it('onTestSkipped',()=>{
-        const suite = {title: 'sample suite',uuid: '1234'}
-        const test = {title: 'this is a test',uuid: '9876'}
+    it("onTestSkipped", () => {
+        const suite = { title: "sample suite", uuid: "1234" };
+        const test = { title: "this is a test", uuid: "9876" };
 
-        reporter.onSuiteStart(suite)
-        reporter.onTestStart(test)
-        expect(reporter.currTest.title).toBe(test.title)
-        expect(reporter.currTest.context[0]).toMatchObject({ title: 'Session Id', value: runner.sessionId })
-    })
+        reporter.onSuiteStart(suite);
+        reporter.onTestStart(test);
+        expect(reporter.currTest.title).toBe(test.title);
+        expect(reporter.currTest.context[0]).toMatchObject({
+            title: "Session Id",
+            value: runner.sessionId,
+        });
+    });
 
-    it('onAfterCommand',()=>{
-        const suite = {title: 'sample suite',uuid: '1234'}
-        const test = {title: 'this is a test',uuid: '9876'}
-        const command = {endpoint: '/session/123456/screenshot/',result: { value: 'abcdefg' }}
-        reporter.onSuiteStart(suite)
-        reporter.onTestStart(test)
+    it("onAfterCommand", () => {
+        const suite = { title: "sample suite", uuid: "1234" };
+        const test = { title: "this is a test", uuid: "9876" };
+        const command = {
+            endpoint: "/session/123456/screenshot/",
+            result: { value: "abcdefg" },
+        };
+        reporter.onSuiteStart(suite);
+        reporter.onTestStart(test);
 
-        reporter.onAfterCommand(command)
-        expect(reporter.currTest.context[0]).toMatchObject({ title: 'Session Id', value: runner.sessionId })
-        expect(reporter.currTest.context[1]).toMatchObject({ title: 'Screenshot', value: 'data:image/jpeg;base64,abcdefg' })
-    })
+        reporter.onAfterCommand(command);
+        expect(reporter.currTest.context[0]).toMatchObject({
+            title: "Session Id",
+            value: runner.sessionId,
+        });
+        expect(reporter.currTest.context[1]).toMatchObject({
+            title: "Screenshot",
+            value: "data:image/jpeg;base64,abcdefg",
+        });
+    });
 
-    it('onTestEnd',()=>{
-        const suite = {title: 'sample suite',uuid: '1234'}
-        const test = {title: 'this is a test',uuid: '9876', _duration: '123', state: 'passed'}
-        reporter.onSuiteStart(suite)
-        reporter.onTestStart(test)
+    it("onTestEnd", () => {
+        const suite = { title: "sample suite", uuid: "1234" };
+        const test = {
+            title: "this is a test",
+            uuid: "9876",
+            _duration: "123",
+            state: "passed",
+        };
+        reporter.onSuiteStart(suite);
+        reporter.onTestStart(test);
 
-        reporter.onTestEnd(test)
-        expect(reporter.currTest.duration).toBe(test._duration)
-        expect(reporter.currTest.pass).toBe(true)
-        expect(reporter.currSuite.tests.length).toBe(1)
-        expect(reporter.results.stats.tests).toBe(1)
-    })
+        reporter.onTestEnd(test);
+        expect(reporter.currTest.duration).toBe(test._duration);
+        expect(reporter.currTest.pass).toBe(true);
+        expect(reporter.currSuite.tests.length).toBe(1);
+        expect(reporter.results.stats.tests).toBe(1);
+    });
 
-    it('onTestEnd - skipped',()=>{
-        const suite = {title: 'sample suite',uuid: '1234'}
-        const test = {title: 'this is a test',uuid: '9876', _duration: '123', state: 'skipped'}
-        reporter.onSuiteStart(suite)
-        reporter.onTestStart(test)
+    it("onTestEnd - skipped", () => {
+        const suite = { title: "sample suite", uuid: "1234" };
+        const test = {
+            title: "this is a test",
+            uuid: "9876",
+            _duration: "123",
+            state: "skipped",
+        };
+        reporter.onSuiteStart(suite);
+        reporter.onTestStart(test);
 
-        reporter.onTestEnd(test)
-        expect(reporter.currTest.duration).toBe(test._duration)
-        expect(reporter.currTest.pending).toBe(true)
-        expect(reporter.currSuite.tests.length).toBe(1)
-        expect(reporter.results.stats.tests).toBe(1)
-    })
+        reporter.onTestEnd(test);
+        expect(reporter.currTest.duration).toBe(test._duration);
+        expect(reporter.currTest.pending).toBe(true);
+        expect(reporter.currSuite.tests.length).toBe(1);
+        expect(reporter.results.stats.tests).toBe(1);
+    });
 
-    it('onSuiteEnd',()=>{
-        const suite = {title: 'sample suite',uuid: '1234', duration: '897'}
-        const test = {title: 'this is a test',uuid: '9876', _duration: '123', state: 'passed'}
-        reporter.onSuiteStart(suite)
-        reporter.onTestStart(test)
-        reporter.onTestEnd(test)
+    it("onSuiteEnd", () => {
+        const suite = { title: "sample suite", uuid: "1234", duration: "897" };
+        const test = {
+            title: "this is a test",
+            uuid: "9876",
+            _duration: "123",
+            state: "passed",
+        };
+        reporter.onSuiteStart(suite);
+        reporter.onTestStart(test);
+        reporter.onTestEnd(test);
 
-        reporter.onSuiteEnd(suite)
+        reporter.onSuiteEnd(suite);
 
-        expect(reporter.currSuite.duration).toBe(suite.duration)
-        expect(reporter.results.stats.suites).toBe(1)
-        expect(reporter.results.suites.suites.length).toBe(1)
-    })
+        expect(reporter.currSuite.duration).toBe(suite.duration);
+        expect(reporter.results.stats.suites).toBe(1);
+        expect(reporter.results.suites.suites.length).toBe(1);
+    });
 
-    it('onRunnerEnd', () =>{
-        reporter.write = jest.fn()
-        runner.end = '1234567890'
-        runner.duration = '9987'
+    it("onRunnerEnd", () => {
+        reporter.write = jest.fn();
+        runner.end = "1234567890";
+        runner.duration = "9987";
 
-        reporter.onRunnerEnd(runner)
-        expect(reporter.results.stats.end).toBe(runner.end)
-        expect(reporter.results.stats.duration).toBe(runner.duration)
-    })
-})
+        reporter.onRunnerEnd(runner);
+        expect(reporter.results.stats.end).toBe(runner.end);
+        expect(reporter.results.stats.duration).toBe(runner.duration);
+    });
+});
--- test/suite.spec.js
+++ test/suite.spec.js
@@ -1,48 +1,48 @@
-import Suite from '../src/suite'
-import Test from '../src/test'
+import Suite from "../src/suite.js";
+import Test from "../src/test.js";
 
-describe('Suite Class Tests',()=>{
-    it('Should successfully instantiate a ROOT suite',()=>{
-        const suite = new Suite(true, {title: ''})
+describe("Suite Class Tests", () => {
+    it("Should successfully instantiate a ROOT suite", () => {
+        const suite = new Suite(true, { title: "" });
 
-        expect(suite.root).toBe(true)
-        expect(suite.rootEmpty).toBe(undefined)
-    })
+        expect(suite.root).toBe(true);
+        expect(suite.rootEmpty).toBe(undefined);
+    });
 
-    it('Should successfully instantiate a NON-ROOT suite',()=>{
-        const sampleSuite = { title: 'Sample Suite' }
-        const sanitizedCaps = 'chrome.74_0_3729_157.macosx'
-        const suite = new Suite(false,sampleSuite, sanitizedCaps)
+    it("Should successfully instantiate a NON-ROOT suite", () => {
+        const sampleSuite = { title: "Sample Suite" };
+        const sanitizedCaps = "chrome.74_0_3729_157.macosx";
+        const suite = new Suite(false, sampleSuite, sanitizedCaps);
 
-        expect(suite.root).toBe(false)
-        expect(suite.rootEmpty).toBe(undefined)
-        expect(suite.title).toContain(sampleSuite.title)
-        expect(suite.title).toContain(sanitizedCaps)
-    })
+        expect(suite.root).toBe(false);
+        expect(suite.rootEmpty).toBe(undefined);
+        expect(suite.title).toContain(sampleSuite.title);
+        expect(suite.title).toContain(sanitizedCaps);
+    });
 
-    it('Should successfully add a suite to the ROOT suite',()=>{
-        const rootSuite = new Suite(true, {title: ''})
-        const sampleSuite = { title: 'Sample Suite' }
-        const sanitizedCaps = 'chrome.74_0_3729_157.macosx'
+    it("Should successfully add a suite to the ROOT suite", () => {
+        const rootSuite = new Suite(true, { title: "" });
+        const sampleSuite = { title: "Sample Suite" };
+        const sanitizedCaps = "chrome.74_0_3729_157.macosx";
 
-        const suite = new Suite(true,rootSuite)
-        suite.addSuite(false,sampleSuite,sanitizedCaps)
+        const suite = new Suite(true, rootSuite);
+        suite.addSuite(false, sampleSuite, sanitizedCaps);
 
-        expect(suite.suites).toHaveLength(1)
-    })
+        expect(suite.suites).toHaveLength(1);
+    });
 
-    it('Should successfully add a test to a NON-ROOT suite',()=>{
-        const sampleSuite = { title: 'Sample Suite' }
-        const sanitizedCaps = 'chrome.74_0_3729_157.macosx'
-        const sampleTest = { title: 'This is a test' }
-        const suite = new Suite(false,sampleSuite, sanitizedCaps)
-        const test = new Test(sampleTest,sampleSuite.uuid)
-        test.updateResult({state: 'passed'})
+    it("Should successfully add a test to a NON-ROOT suite", () => {
+        const sampleSuite = { title: "Sample Suite" };
+        const sanitizedCaps = "chrome.74_0_3729_157.macosx";
+        const sampleTest = { title: "This is a test" };
+        const suite = new Suite(false, sampleSuite, sanitizedCaps);
+        const test = new Test(sampleTest, sampleSuite.uuid);
+        test.updateResult({ state: "passed" });
 
-        suite.addTest(test)
-        expect(suite.tests.length).toBe(1)
-        expect(suite.passes.length).toBe(1)
-        expect(suite.passes).toContain(test.uuid)
-        expect(suite.tests[0].uuid).toBe(test.uuid)
-    })
-})
+        suite.addTest(test);
+        expect(suite.tests.length).toBe(1);
+        expect(suite.passes.length).toBe(1);
+        expect(suite.passes).toContain(test.uuid);
+        expect(suite.tests[0].uuid).toBe(test.uuid);
+    });
+});
